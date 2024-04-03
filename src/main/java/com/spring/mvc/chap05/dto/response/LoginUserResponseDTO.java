a995326044b533f3722de223002f6ea48e1f1baa
gayeon <ini03134@naver.com>
2024-04-03 17:42:48 +0900

    #19. 회원관리 로그인 처리, 쿠키 사용

---
+++ src/main/java/com/spring/mvc/chap05/dto/response/LoginUserResponseDTO.java
@@ -0,0 +1,20 @@
+package com.spring.mvc.chap05.dto.response;
+
+
+import jdk.jfr.StackTrace;
+import lombok.*;
+
+@Getter @StackTrace @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginUserResponseDTO {
+
+    private String account;
+    private String name;
+    private String email;
+
+
+
+}
