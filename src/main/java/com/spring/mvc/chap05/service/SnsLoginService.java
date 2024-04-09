a87e36b29068c2cfeedd67083ca53ecde16d206d
gayeon <ini03134@naver.com>
2024-04-09 13:24:18 +0900

    #37. 카카오 로그인 신청 및 인가코드 받기

---
+++ src/main/java/com/spring/mvc/chap05/service/SnsLoginService.java
@@ -0,0 +1,13 @@
+package com.spring.mvc.chap05.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class SnsLoginService {
+
+    public void kakaoLogin(Map<String, String> params) {
+
+    }
+}
